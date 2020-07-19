29efedfebaef79c93e9d893320aac89153f4f379
kyle-law <junwei_1121@hotmail.com>
2020-07-19 16:42:32 +0800

    Updates

---
+++ precache-manifest.0ff99ad25343a3b3d266d71f4be81841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201fb02ed633d8041f93faf0ddf11502",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "825a678aa479ff3e789e",
+    "url": "/expense-tracker/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825a678aa479ff3e789e",
+    "url": "/expense-tracker/static/js/main.a5d4cdaf.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
